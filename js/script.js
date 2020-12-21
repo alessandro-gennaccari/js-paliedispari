c4a301f68dc83c4e639ab391d003125f23293d6a
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2020-12-21 13:48:56 +0100

    function randomNumber and somma + fix numberuser

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ Dichiariamo chi ha vinto. */
 
 var sceltaUtente = prompt('Scegli tra pari o dispari.');
 sceltaUtente = sceltaUtente.toLowerCase();
-var numeroUtente = prompt('Scegli un numero da 1 a 5.')
+var numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5.'))
 
 while (sceltaUtente != 'pari' && sceltaUtente != 'dispari'){
     scelta = prompt('Hai sbagliato, puoi scegliere tra "pari" o "dispari".')
@@ -15,6 +15,21 @@ while (sceltaUtente != 'pari' && sceltaUtente != 'dispari'){
 console.log(sceltaUtente);
 
 while (numeroUtente <= 0 || numeroUtente >= 6){
-    numeroUtente = prompt('Hai sbagliato numero, puoi scegliere un numero da "1" a "5".')
+    numeroUtente = parseInt(prompt('Hai sbagliato numero, puoi scegliere un numero da "1" a "5".'))
 }
 console.log(numeroUtente);
+
+var numeroCpu = randomNumber(1,5);
+var sommaNumeri = somma(numeroUtente,numeroCpu)
+console.log(numeroCpu);
+console.log(sommaNumeri);
+
+
+// Funzioni
+function randomNumber(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function somma(n1,n2) {
+    return n1 + n2;
+}
